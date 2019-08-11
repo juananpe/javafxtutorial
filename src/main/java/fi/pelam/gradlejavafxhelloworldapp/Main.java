32680b128b1d19f997f5ff372893748adc6a8f7a
Juanan Pereira <juanan@ikasten.io>
2019-08-11 18:55:38 +0200

    add stylesheets

--- src/main/java/fi/pelam/gradlejavafxhelloworldapp/Main.java
+++ src/main/java/fi/pelam/gradlejavafxhelloworldapp/Main.java
@@ -1,13 +1,11 @@
 package fi.pelam.gradlejavafxhelloworldapp;
 
 import javafx.application.Application;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -18,10 +16,22 @@ public class Main extends Application {
     public void start(Stage stage) {
 
         VBox vBox = new VBox(new Label("A JavaFX Label"));
+
+        Button button1 = new Button("Button 1");
+        Button button2 = new Button("Button 2");
+
+        vBox.getChildren().add(button1);
+        vBox.getChildren().add(button2);
+
+        vBox.getStylesheets().add(getClass().getResource("button-styles.css").toExternalForm());
+
+//        vBox.getStylesheets().add("button-styles.css");
+
         Scene scene = new Scene(vBox);
 
         stage.setTitle("JavaFX Stage Window Title");
         stage.setScene(scene);
+
 
         // scene.getRoot().setCursor(Cursor.CROSSHAIR);
 
