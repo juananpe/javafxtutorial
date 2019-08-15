8e8c8cafe109308108f420a8bb22d181938fb349
Juanan Pereira <juanan@ikasten.io>
2019-08-15 13:14:46 +0200

    add buttons and css example

--- src/main/java/fi/pelam/gradlejavafxhelloworldapp/ButtonExperiments.java
+++ src/main/java/fi/pelam/gradlejavafxhelloworldapp/ButtonExperiments.java
@@ -1,46 +1,39 @@
 package fi.pelam.gradlejavafxhelloworldapp;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.*;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 public class ButtonExperiments extends Application  {
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("HBox Experiment 1");
+        primaryStage.setTitle("Button Experiment 1");
 
-        Label label = new Label("Not clicked");
-        // beware macOS users...
-        // https://stackoverflow.com/a/24500058/243532
-        Button button = new Button();
-        button.setMnemonicParsing(true);
-        button.setText("_Click");
+        Button button1 = new Button("Button 1");
+        Button button2 = new Button("Button 2");
+        Button button3 = new Button("Button 3");
+        Button button4 = new Button("Button 4");
 
-        // alternative accelerator keys for macOS users
-        KeyCombination kc = new KeyCodeCombination(KeyCode.C, KeyCombination.ALT_DOWN);
-        Runnable task = () -> button.fire();
+        button1.setId("button1"); // this button gets its style from an external css
+        button2.setStyle("-fx-background-color: #00ff00");
+        button3.setStyle("-fx-font-size: 2em; ");
+        button4.setStyle("-fx-text-fill: #0000ff");
 
-        button.setOnAction(value ->  {
-           label.setText("Clicked!");
-        });
 
-        HBox hbox = new HBox(button, label);
+        HBox hbox = new HBox(button1, button2, button3, button4);
+        hbox.getStylesheets().add(getClass().getResource("/button-styles.css").toExternalForm());
 
-        Scene scene = new Scene(hbox, 200, 100);
-        // macOS users
-        scene.getAccelerators().put(kc, task);
-
+        Scene scene = new Scene(hbox, 400, 100);
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
