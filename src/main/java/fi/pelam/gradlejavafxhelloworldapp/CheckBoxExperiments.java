5e23b39b7dd2f842ed03a5a5faa8d468b1c6841d
Juanan Pereira <juanan@ikasten.io>
2019-08-26 16:36:37 +0200

    add CheckBoxExperiments

---
+++ src/main/java/fi/pelam/gradlejavafxhelloworldapp/CheckBoxExperiments.java
@@ -0,0 +1,36 @@
+package fi.pelam.gradlejavafxhelloworldapp;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+
+public class CheckBoxExperiments extends Application  {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("CheckBox Experiment 1");
+
+        CheckBox checkBox1 = new CheckBox("Green");
+
+        System.out.print( checkBox1.isSelected());
+        checkBox1.setOnAction(e -> {
+            System.out.print( checkBox1.isSelected());
+        });
+
+        HBox hbox = new HBox(checkBox1);
+
+        Scene scene = new Scene(hbox, 200, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
